a767ce68f72f0c5c013c91ea8147e395a9ef59b5
Dat Van <vanthanhphuongdat1309@gmail.com>
2022-08-04 22:46:13 +0700

    split input components

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,6 +1,9 @@
 import React from "react";
 import styled from "styled-components";
+import { useForm } from "react-hook-form";
+
 import { Label } from "components/label";
+import { Input } from "components/input";
 
 const StyledSignUpPage = styled.div`
   min-height: 100vh;
@@ -29,46 +32,34 @@ const StyledSignUpPage = styled.div`
     align-items: flex-start;
     row-gap: 20px;
   }
-
-  .input {
-    width: 100%;
-    padding: 20px;
-    background-color: ${(props) => props.theme.greyLight};
-    border-radius: 8px;
-    font-weight: 500;
-    transition: all 0.2s linear;
-    border: 1px solid transparent;
-  }
-
-  .input:focus {
-    background-color: white;
-    border-color: ${(props) => props.theme.primary};
-  }
-
-  .input::-webkit-input-placeholder {
-    color: #84878b;
-  }
-
-  .input::-moz-input-placeholder {
-    color: #84878b;
-  }
 `;
 
 const SignUpPage = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isValid, isSubmitting },
+    watch,
+  } = useForm({});
+
+  const handleSignUp = (values) => {
+    console.log(values);
+  };
+
   return (
     <StyledSignUpPage>
       <div className="container">
         <img srcSet="/logo.png 2x" alt="monkey-blogging" className="logo" />
         <h1 className="heading">Monkey Blogging</h1>
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit(handleSignUp)}>
           <div className="field">
             <Label htmlFor="fullname">Fullname</Label>
-            <input
-              id="fullname"
+            <Input
+              name="fullname"
               type="text"
-              className="input"
               placeholder="Enter your fullname"
-            ></input>
+              control={control}
+            ></Input>
           </div>
         </form>
       </div>
