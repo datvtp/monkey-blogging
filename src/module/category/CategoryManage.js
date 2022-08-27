f703e7da1957e9f22e4ce41e6644b82774a6bad8
Dat Van <vanthanhphuongdat1309@gmail.com>
2022-08-28 02:20:35 +0700

    filter

--- src/module/category/CategoryManage.js
+++ src/module/category/CategoryManage.js
@@ -1,13 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
+import { debounce } from "lodash";
+import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { collection, deleteDoc, doc, onSnapshot } from "firebase/firestore";
 
 import { Table } from "components/table";
 import DashboardHeading from "module/dashboard/DashboardHeading";
 import { Button } from "components/button";
 import { LabelStatus } from "components/label";
 import { ActionDelete, ActionEdit, ActionView } from "components/action";
-import { collection, deleteDoc, doc, onSnapshot } from "firebase/firestore";
 import { db } from "firebase-app/firebase-config";
 import { categoryStatus, theme } from "utils/constants";
 
@@ -15,20 +16,33 @@ const CategoryManage = () => {
   const navigate = useNavigate();
 
   const [categories, setCategories] = useState([]);
+  const [filter, setFilter] = useState("");
 
   useEffect(() => {
     const colRef = collection(db, "categories");
+
     onSnapshot(colRef, (snapshot) => {
       const results = [];
       snapshot.forEach((doc) => {
-        results.push({
-          id: doc.id,
-          ...doc.data(),
-        });
+        if (filter) {
+          if (doc.data().name.includes(filter)) {
+            results.push({
+              id: doc.id,
+              ...doc.data(),
+            });
+          }
+        } else {
+          results.push({
+            id: doc.id,
+            ...doc.data(),
+          });
+        }
       });
       setCategories(results);
     });
-  }, []);
+  }, [filter]);
+
+  const handleFilterOnChange = debounce((e) => setFilter(e.target.value), 500);
 
   const handleUpdate = (categoryId) => {
     navigate(`/manage/update-category?id=${categoryId}`);
@@ -56,15 +70,23 @@ const CategoryManage = () => {
   return (
     <div>
       <DashboardHeading title="Categories" desc="Manage your category">
-        <Button
-          className="max-w-[200px]"
-          variant="secondary"
-          onClick={() => {
-            navigate("/manage/add-category");
-          }}
-        >
-          Create category
-        </Button>
+        <div className="flex items-center gap-x-5">
+          <input
+            type="text"
+            placeholder="Search category..."
+            className="h-[52px] px-5 border-solid border-2 border-gray-300 focus:border-primary rounded-lg"
+            onChange={handleFilterOnChange}
+          ></input>
+          <Button
+            className="max-w-[200px]"
+            variant="secondary"
+            onClick={() => {
+              navigate("/manage/add-category");
+            }}
+          >
+            Create category
+          </Button>
+        </div>
       </DashboardHeading>
       <Table>
         <thead>
